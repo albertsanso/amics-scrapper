ffba6121ae0bc69474bb34a576f038a7546ab462
Albert Sanso <albertsanso@gmail.com>
2019-12-23 01:42:28 +0100

    changes

---
+++ scrapper-runtime/src/main/java/org/tttamics/OrganizationCreatedEventSubscriber.java
@@ -0,0 +1,15 @@
+package org.tttamics;
+
+import org.albertsanso.commons.event.DomainEventSubscriber;
+import org.tttamics.scrapper.core.domain.event.OrganizationCreatedEvent;
+
+import javax.inject.Named;
+
+@Named
+public class OrganizationCreatedEventSubscriber extends DomainEventSubscriber<OrganizationCreatedEvent> {
+    @Override
+    public void handle(OrganizationCreatedEvent organizationCreatedEvent) {
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> OrganizationCreatedEventHandler");
+    }
+}
